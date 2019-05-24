a33e77dc945f0cf5d1f86a8c068994b2252d200b
jinbinhu <45911482+jinbinhu@users.noreply.github.com>
2019-05-25 04:44:53 +0800

    Update CAPS-dark.sh

--- CAPS-dark.sh
+++ CAPS-dark.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+#Installing
 tar -xvzf  ns-allinone-2.35.tar.gz
 cd ns-allinone-2.35
 ./install
 
+#Patching
 cp ../CAPS-dark.patch ./
-
 patch -p5 < CAPS-dark.patch
 
+#Compiling
 cd ns-2.35
 sudo make
 
