762de21e091c873303382d12d6374440cb19a131
jinbinhu <45911482+jinbinhu@users.noreply.github.com>
2019-05-25 04:45:41 +0800

    Update CAPS.sh

--- CAPS.sh
+++ CAPS.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+#Installing
 tar -xvzf  ns-allinone-2.35.tar.gz
 cd ns-allinone-2.35
 ./install
 
+#Patching
 cp ../CAPS.patch ./
-
 patch -p5 < CAPS.patch   
 
+#Compiling
 cd ns-2.35
 sudo make
 
