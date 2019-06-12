3739bfb36b01bec0281527363beefff439b6ae0a
jinbinhu <45911482+jinbinhu@users.noreply.github.com>
2019-06-12 10:45:14 +0800

    Update NS2.sh

--- NS2.sh
+++ NS2.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 #Installing NS-2.35
+sudo apt-get update && sudo apt-get install vim
+sudo apt-get install libx11-dev
 sudo apt-get install build-essential
 sudo apt-get install tcl8.4 tcl8.4-dev
 sudo apt-get install tk8.4-dev
-sudo apt-get install libxmu-dev
+sudo apt-get install libxmu-dev libxmu-headers
 tar -xvzf  ns-allinone-2.35.tar.gz
 cd ns-allinone-2.35
-cp ../ls.h ./linkstate/
+cp ../ls.h  ./ns-2.35/linkstate/
 ./install
 
 
